3f8b8ae4f47b52273916b25705445f99abe8e2c5
Tomek <sigma345@wp.pl>
2025-12-06 21:45:22 +0100

    add route to' "/" and "/addUser" path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import ShowUser from './getUser/ShowUser';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import AddUser from './addUser/AddUser';
 
 function App() {
+ const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <ShowUser />
+
+  },
+  {
+    path: "/addUser",
+    element: <AddUser />
+  }
+ ]);
+
   return (
     <div className="App">
-      <ShowUser />
+      <RouterProvider router={route}></RouterProvider>
+  
       </div>
   );
 }
