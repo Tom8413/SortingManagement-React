39dda1e7490278deaa7206b2ab80c7d44ba16e7c
Tomek <sigma345@wp.pl>
2025-12-07 17:44:36 +0100

    change path to showUser.css syles

--- frontend/src/getUser/ShowUser.jsx
+++ frontend/src/getUser/ShowUser.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "../getUser/user.css";
+import "../getUser/showUser.css";
 import { Link } from "react-router-dom";
 
 const ShowUser = () => {
