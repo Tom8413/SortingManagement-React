ee0a01aba84020984d63002897a4783ea6732c70
Tomek <sigma345@wp.pl>
2025-12-07 17:43:34 +0100

    add form to showUser

--- frontend/src/addUser/AddUser.jsx
+++ frontend/src/addUser/AddUser.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React from 'react';
+import '../addUser/addUser.css';
 
 export const AddUser = () => {
   return (
-    <div>AddUser site</div>
+    <div className="backdrop">
+ 
+    <form>
+    <h3 className='NewUser'>Add New user</h3>
+      <label htmlFor='first_name'>First Name:</label>
+      <input type="text" 
+             id='first_name'
+             required
+             placeholder='Enter First Name' 
+             maxLength="15" 
+             />
+
+      <label htmlFor='last_name'>Last Name:</label>
+      <input type="text"
+             id='last_name'
+             maxLength="15"  
+             placeholder='Enter Last Name'
+             required 
+             />
+
+      <label htmlFor='ID_number'>ID_number:</label>
+      <input type="text"
+             //onkeypress='return event.charCode >= 48 && event.charCode <= 57'
+             maxLength="8" 
+             id='ID_number'
+             placeholder='ID_number'
+             required
+             />
+
+      <label htmlFor='Department'>Department: </label>
+      <select >
+        <option value="NCP">NCP</option>
+        <option value="NCC">NCC</option>
+        <option value="STM">STM</option>
+        <option value="Kids">Kids</option>
+      </select>
+    
+      <button type="button">      
+              Submit
+      </button>
+      <button type="button">Cancel</button>
+    </form>
+  </div>
   )
 }
 
