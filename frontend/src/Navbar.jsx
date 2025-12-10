271aa78270c5c9524a4ea07ac6a6ccc11b051e65
Tomek <sigma345@wp.pl>
2025-12-10 16:30:07 +0100

    add NavbarLink to userList

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import "./Navbar.css";
+import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
-    <nav className='navbar'>
-
-    </nav>
+    <><NavLink to="/userList"
+               className="navlink-left">
+        <h3>Users List</h3>
+        
+    </NavLink>
+    </>
   );
 };
 
