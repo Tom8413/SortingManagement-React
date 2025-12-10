03d91df1d9f3c4759dece019554515fbe700ac9d
Tomek <sigma345@wp.pl>
2025-12-10 16:28:38 +0100

    add Navbar component, new path to UserList

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,30 @@ import './App.css';
 import ShowUser from './getUser/ShowUser';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import AddUser from './addUser/AddUser';
+import UserList from './userList/UserList';
+import Navbar from './Navbar';
 
 function App() {
+
  const route = createBrowserRouter([
   {
     path: "/",
-    element: <ShowUser />
+    element: [<Navbar />, <ShowUser />]
 
   },
   {
     path: "/addUser",
     element: <AddUser />
-  }
+  },
+  {
+    path: "/userList",
+    element: <UserList />
+  },
  ]);
 
   return (
     <div className="App">
       <RouterProvider router={route}></RouterProvider>
-  
       </div>
   );
 }
