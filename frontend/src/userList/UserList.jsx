4dc9f1ad8d67be1b01e09b9cbc6161423e90a6c5
Tomek <sigma345@wp.pl>
2025-12-11 21:03:15 +0100

    add tabel to userlist

--- frontend/src/userList/UserList.jsx
+++ frontend/src/userList/UserList.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
-export const UserList = () => {
+export const UserList = ({users}) => {
   return (
-    <div>userList</div>
-  )
-}
+<>
+    <div className="App">
+        <table>
+            <tbody>
+            <tr>
+                <th>first_name</th>
+                <th>last_name</th>
+                <th>ID_number</th>
+                <th>Department</th>
+            </tr>
+            </tbody>
+            {users.map((user, index) => {
+                return (
+                    <tbody>
+                    <tr>
+                        <td>{user.first_name}</td>
+                        <td>{user.last_name}</td>
+                        <td>{user.ID_number}</td>
+                        <td>{user.Department}</td>
+                    </tr>
+                    </tbody>
+                )
+            })}
+        </table>
+    </div>
+    </>
+  );
+ 
+};
 
 export default UserList;
