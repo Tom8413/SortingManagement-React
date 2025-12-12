d547d21a22d0fce2725b9c3335e6ce29e24ceb9c
Tomek <sigma345@wp.pl>
2025-12-12 22:02:32 +0100

    remove toast from imports

--- frontend/src/getUser/ShowUser.jsx
+++ frontend/src/getUser/ShowUser.jsx
@@ -2,10 +2,9 @@ import React, { useEffect } from "react";
 import axios from "axios";
 import "../getUser/showUser.css";
 import { Link } from "react-router-dom";
-//import toast from "react-hot-toast";
 import Navbar from "../Navbar";
 
-const ShowUser = ({users, setUsers, deleteUser}) => {
+const ShowUser = ({ users, setUsers, deleteUser }) => {
 
 
     useEffect(() => {
@@ -24,28 +23,28 @@ const ShowUser = ({users, setUsers, deleteUser}) => {
 
     return (
         <> <Navbar />
-        <div className="horizontal">
-            {users.map((user, index) => {
-                return (
-                    <div className="block" key={index}>
-                        <div>First name: {user.first_name}</div>
-                        <div>Last name: {user.last_name}</div>
-                        <div>ID number: {user.ID_number}</div>
-                        <div>Department: {user.Department}</div>
-                        <button
-                        onClick={() => deleteUser(user._id)}
-                        >Delete</button>
-                    </div>
-                );
-            })}
-        </div>
-        <Link to="/addUser">
-            <button> Add User</button>
-        </Link>
-       
+            <div className="horizontal">
+                {users.map((user, index) => {
+                    return (
+                        <div className="block" key={index}>
+                            <div>First name: {user.first_name}</div>
+                            <div>Last name: {user.last_name}</div>
+                            <div>ID number: {user.ID_number}</div>
+                            <div>Department: {user.Department}</div>
+                            <button
+                                onClick={() => deleteUser(user._id)}
+                            >Delete</button>
+                        </div>
+                    );
+                })}
+            </div>
+            <Link to="/addUser">
+                <button> Add User</button>
+            </Link>
+
         </>
-        
-        
+
+
     )
 }
 
