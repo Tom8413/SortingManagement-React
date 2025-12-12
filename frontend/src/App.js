c32ad27c83652eadf5121ac83cc0ba995a513832
Tomek <sigma345@wp.pl>
2025-12-12 20:16:53 +0100

    add deleteUser function and end to child

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,47 @@
 import './App.css';
 import ShowUser from './getUser/ShowUser';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AddUser from './addUser/AddUser';
 import UserList from './userList/UserList';
 import React, { useState } from "react";
+import axios from 'axios';
+import toast from "react-hot-toast";
 
 function App() {
 
   const [users, setUsers] = useState([]);
- 
 
- const route = createBrowserRouter([
-  {
-    path: "/",
-    element: <ShowUser users={users} setUsers={setUsers}/>
+  const deleteUser = async (userId) => {
+    await axios.delete(`http://localhost:8000/delete-employee/${userId}`)
+      .then((response) => {
+        setUsers((prevUser) => prevUser.filter((user) => user._id !== userId));
+        toast.success("User " + response.data.first_name + " deleted successful!", { position: "top-right" });
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
 
-  },
-  {
-    path: "/addUser",
-    element: <AddUser />
-  },
-  {
-    path: "/userList",
-    element: <UserList users={users} setUsers={setUsers}/>
-  },
- ]);
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <ShowUser users={users} setUsers={setUsers} deleteUser={deleteUser}/>
+
+    },
+    {
+      path: "/addUser",
+      element: <AddUser />
+    },
+    {
+      path: "/userList",
+      element: <UserList users={users} setUsers={setUsers} deleteUser={deleteUser}/>
+    },
+  ]);
 
   return (
     <div className="App">
       <RouterProvider router={route}></RouterProvider>
-      </div>
+    </div>
   );
 }
 
