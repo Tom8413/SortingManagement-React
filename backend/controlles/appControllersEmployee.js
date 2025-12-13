c034db9d6f14f72422b83b70001dd9fb5a5bcc4d
Tomek <sigma345@wp.pl>
2025-12-13 20:16:18 +0100

    add employee2 variable and function to send data

--- backend/controlles/appControllersEmployee.js
+++ backend/controlles/appControllersEmployee.js
@@ -1,5 +1,5 @@
 const Employees = require('../modules/ShameTemplate');
-
+const Employees2 = require('../modules/ShameTemplate2');
 
 
 const employee_details = (req, res) => {
@@ -10,13 +10,22 @@ const employee_details = (req, res) => {
         .catch((error) => {
             res.status(500).json(error)
         })
-    };
+};
 
-    
+
 const create_employee = (req, res) => {
     const employee = new Employees(req.body);
+    const employee2 = new Employees2(req.body);
 
     employee.save()
+        .then((result) => {
+            res.status(201).send(result)
+            console.log(req.body)
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    employee2.save()
         .then((result) => {
             res.status(201).send(result)
             console.log(req.body)
@@ -29,14 +38,14 @@ const create_employee = (req, res) => {
 const delete_employee = (req, res) => {
     const id = req.params.id;
     Employees.findByIdAndDelete(id)
-    .then(result => {
-        res.status(200).send(result)
-        console.log(result);
-    })
-    .catch(err => {
-        res.status(500);
-        console.log(err);
-    })
+        .then(result => {
+            res.status(200).send(result)
+            console.log(result);
+        })
+        .catch(err => {
+            res.status(500);
+            console.log(err);
+        })
 }
 
 module.exports = {
