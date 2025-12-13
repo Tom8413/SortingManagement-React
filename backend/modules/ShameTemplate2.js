fe5e5eb9f381580b29dcfe9e8663209502310d51
Tomek <sigma345@wp.pl>
2025-12-13 20:16:44 +0100

    add ShamaTemplate2

---
+++ backend/modules/ShameTemplate2.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const employeesSchema = new Schema({
+    first_name: {
+        type: String,
+        Required: true
+    },
+    last_name: {
+        type: String,
+        Required: true
+    },
+    ID_number: {
+        type: Number,
+        Required: true
+    },
+    Department: {
+        type: String,
+        Required: true
+    }
+}, {timestamps: true});
+
+const Employees2 = mongoose.model('Employee2', employeesSchema);
+module.exports = Employees2; 
