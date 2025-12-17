e4ed63df08f515d7a02b696f1cf9f03150a74bbb
Tomek <sigma345@wp.pl>
2025-12-17 19:03:07 +0100

    add useeffect get method to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,12 @@ import UserList from './userList/UserList';
 import React, { useState } from "react";
 import axios from 'axios';
 import toast from "react-hot-toast";
+import { useEffect } from 'react'; 
 
 function App() {
 
   const [users, setUsers] = useState([]);
+  const [users2, setUsers2] = useState([]);
 
   const deleteUser = async (userId) => {
     await axios.delete(`http://localhost:8000/delete-employee/${userId}`)
@@ -20,7 +22,48 @@ function App() {
       .catch((error) => {
         console.log(error);
       })
-  }
+  };
+
+const url = "http://localhost:8000/show-employee";
+
+  useEffect(() => {
+    const fetchData = async (setUsers, url) => {
+        try {
+            const response = await axios.get(url);
+            setUsers(response.data);
+        } catch (error) {
+            console.log(error);
+  
+        }
+    };
+    fetchData()
+  }, [setUsers]);
+
+/*  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await axios.get("http://localhost:8000/show-employee");
+            setUsers(response.data);
+        } catch (error) {
+            console.log(error);
+  
+        }
+    };
+    fetchData()
+  }, [setUsers]);
+*/
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await axios.get("http://localhost:8000/show-employee2");
+            setUsers2(response.data);
+        } catch (error) {
+            console.log(error);
+
+        }
+    };
+    fetchData()
+}, [setUsers2]);
 
   const route = createBrowserRouter([
     {
@@ -34,7 +77,7 @@ function App() {
     },
     {
       path: "/userList",
-      element: <UserList users={users} setUsers={setUsers} deleteUser={deleteUser}/>
+      element: <UserList users2={users2} setUsers2={setUsers2} deleteUser={deleteUser}/>
     },
   ]);
 
