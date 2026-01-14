f2db6e4bccf5e46ca84358f17efee7902ae26197
Tomasz <sigma345@wp.pl>
2026-01-14 17:38:14 +0100

    remove Navbar component, move button and Link

--- frontend/src/getUser/ShowUser.jsx
+++ frontend/src/getUser/ShowUser.jsx
@@ -1,10 +1,13 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import "../getUser/showUser.css";
-import Navbar from "../Navbar";
+//import Navbar from "../Navbar";
+import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 const ShowUser = ({ users, setUsers, deleteUser }) => {
 
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchData = async () => {
@@ -21,28 +24,44 @@ const ShowUser = ({ users, setUsers, deleteUser }) => {
 
 
     return (
-        <> <Navbar />
+        <>
             <div className="horizontal">
-                {users.map((user, index) => {
-                    if(index < 8) {
-                    return (
-                        <div className="block" key={index}>
-                            <div>First name: {user.first_name}</div>
-                            <div>Last name: {user.last_name}</div>
-                            <div>ID number: {user.ID_number}</div>
-                            <div>Department: {user.Department}</div>
-                            <button
-                                onClick={() => deleteUser(user.ID_number)}
-                            >Delete</button>
-                        </div>
-                    ); 
-                    } else {
-                       return  null
-                    }
-                })}
-                
+                <div>
+                    <div className="navbarbuttonContainer">
+                    <Link to="/userlist">
+                        Database Employee
+                    </Link>
+                    <div  className="buttonContainer">
+                        <button type="button" onClick={() => navigate("/addUser")}> Add User</button>
+                    </div>
+                    </div>
+
+
+                </div>
+                <div className="departmentData">
+                    {users.map((user, index) => {
+                        if (index < 8) {
+                            return (
+                                <div className="block" key={index}>
+                                    <div>First name: {user.first_name}</div>
+                                    <div>Last name: {user.last_name}</div>
+                                    <div>ID number: {user.ID_number}</div>
+                                    <div>Department: {user.Department}</div>
+                                    <button
+                                        onClick={() => deleteUser(user.ID_number)}
+                                    >Delete</button>
+                                </div>
+                            );
+                        } else {
+                            return null
+                        }
+                    })}
+                </div>
+
+                <div>
+                </div>
             </div>
-                <button type="button" onClick={() => "/addUser"}> Add User</button>
+
 
         </>
 
