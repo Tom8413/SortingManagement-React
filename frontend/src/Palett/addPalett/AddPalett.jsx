aa03a5f7bc6f72ddca8ec14b870b602543efda99
Tomasz <sigma345@wp.pl>
2026-02-23 13:03:33 +0100

    add addPalett.jsx

---
+++ frontend/src/Palett/addPalett/AddPalett.jsx
@@ -0,0 +1,157 @@
+import React, { useState } from 'react';
+import '../addPalett/addPalett.css';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import toast from 'react-hot-toast'
+
+
+export const AddPalett = (props) => {
+
+  const [KeyPallet, setKeyPallet] = useState('');
+  const [Quantity, setQuantity] = useState('');
+  const [Department, setDepartment] = useState('NCP');
+  const navigate = useNavigate();
+
+
+  const isFromValid = () => {
+    return KeyPallet.length >= 3 &&
+    Quantity.length >= 7
+
+  };
+console.log(props.sendIndexPalett);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+
+    const data = {
+      ID_number: props.users[props.sendIndexPalett].ID_number,
+      KeyPallet: KeyPallet,
+      Quantity: Quantity,
+      Department: Department
+    };
+
+    let condition = props.users.filter((user) => (user.ID_number) === (event.target.ID_number.value));
+    let condition2 = props.users2.filter((user) => user.ID_number === (event.target.ID_number.value));
+
+    if (condition.length === 0 && condition2.length === 0) {
+      if (props.users.length < 8) {
+        await axios.post("http://localhost:8000/create-employee", data)
+          .then((response) => {
+            console.log(response.data);
+            toast.success("User " + response.data.last_name + " created successful!", { position: "top-right" });
+            navigate("/");
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+
+      } else {
+        toast.error("You cannot add more than nine employees", { position: "top-right" });
+      }
+      await axios.post("http://localhost:8000/create-employee2", data)
+        .then((response2) => {
+          console.log(response2.data);
+          toast.success("User " + response2.data.last_name + " created in database successful!", { position: "top-right" });
+          navigate("/");
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+
+    } else
+      if (condition.length !== 0 && condition2.lenght !== 0) {
+
+        toast.error("User already exist in Department", { position: "top-right" });
+        toast.error("User already exist in DataBase", { position: "top-right" });
+
+      } else if (condition.length !== 0 && condition2.lenght === 0) {
+        toast.error("User already exist in Department", { position: "top-right" });
+
+        await axios.post("http://localhost:8000/create-employee2", data)
+          .then((response2) => {
+            console.log(response2.data);
+            toast.success("User " + response2.data.last_name + " created in database successful!", { position: "top-right" });
+            navigate("/");
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+
+      } else if (condition.length === 0 && condition2.length !== 0) {
+        toast.error("User already exist in DataBase", { position: "top-right" });
+
+        if (props.users.length < 8) {
+          await axios.post("http://localhost:8000/create-employee", data)
+            .then((response) => {
+              console.log(response.data);
+              toast.success("User " + response.data.last_name + " created successful!", { position: "top-right" });
+              navigate("/");
+            })
+            .catch((error) => {
+              console.log(error);
+            })
+        }
+      }
+  }
+
+  return (
+    <div className="backdrop">
+
+      <form onSubmit={handleSubmit}>
+        <h3 className='NewPalett'>Add New Palett</h3>
+        <div className='labelClass'>
+          <h3 clsssName="ID_numberh3" >ID_Number: {props.users[props.sendIndexPalett].ID_number}</h3>
+        </div>
+
+        <div className='labelClass'>
+          <label htmlFor='KeyPallet'>KeyPallet:</label>
+        </div>
+        <input type="text"
+          id='KeyPallet'
+          name='KeyPallet'
+          value={KeyPallet}
+          maxLength={15}
+          placeholder='Enter KeyPallet'
+          onChange={(event) => setKeyPallet(event.target.value.replace(/[^a-z]/gi, ''))}
+        />
+
+        <div className='labelClass'>
+          <label htmlFor='Quantity'>Quantity:</label>
+        </div>
+        <input type="text"
+          maxLength={8}
+          id='Quantity'
+          name='Quantity'
+          value={Quantity}
+          placeholder='Enter Quantity'
+          onChange={(event) => setQuantity(event.target.value.replace(/[^0-9]/gi, ''))}
+        />
+
+        <div className='labelClass'>
+          <label htmlFor='Department'>Department: </label>
+        </div>
+
+        <select
+          className='select'
+          value={Department}
+          onChange={(event) => setDepartment(event.target.value)}>
+          <option value="NCP">NCP</option>
+          <option value="NCC">NCC</option>
+          <option value="STM">STM</option>
+          <option value="Kids">Kids</option>
+        </select>
+        <button type="submit"
+          disabled={!isFromValid()}>
+          Submit
+        </button>
+
+        <button type='button' onClick={() => navigate("/")}>
+          Cancel
+        </button>
+
+      </form>
+    </div>
+  )
+}
+
+export default AddPalett;
