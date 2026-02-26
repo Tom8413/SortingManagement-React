f097145db7720f01c73238f3583e5a31ed290406
Tomasz <sigma345@wp.pl>
2026-02-26 19:55:35 +0100

    addd fetchdata in ShowUser and palett map

--- frontend/src/user/getUser/ShowUser.jsx
+++ frontend/src/user/getUser/ShowUser.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import AddCircleOutlineSharpIcon from '@mui/icons-material/AddCircleOutlineSharp';
 import {Button} from '@mui/material';
 
-const ShowUser = ({ users, setUsers, deleteUser, setsendIndexPalett}) => {
+const ShowUser = ({ users, setUsers, palett, setPalett, deleteUser, setsendIndexPalett}) => {
 
     const navigate = useNavigate();
 
@@ -20,8 +20,17 @@ const ShowUser = ({ users, setUsers, deleteUser, setsendIndexPalett}) => {
 
             }
         };
-        fetchData()
-    }, [setUsers]);
+        const fetchData2 = async () => {
+            try {
+                const response2 = await axios.get("http://localhost:8000/show-EuroPallet")
+                setPalett(response2.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData();
+        fetchData2();
+    }, [setUsers, setPalett]);
 
 
     return (
@@ -64,6 +73,23 @@ const ShowUser = ({ users, setUsers, deleteUser, setsendIndexPalett}) => {
                     })}
                 </div>
                 <div className="departamentPalettData">
+                    {palett.map((palett, index2) => {
+                        if(index2 < 8) {
+                            return (
+                                <div className="block" key={index2}>
+                                    <div className="IconButton">ID_number: {palett.ID_number}</div>
+                                    <div>Keypalett: {palett.KeyPalett}</div>
+                                    <div>Quantity: {palett.Quantity}</div>
+                                    <div>Nestet: {palett.Nestet}</div>
+                                    <div>Department: {palett.Department}</div>
+                                    <div>Location: {palett.Location}</div>
+                                  
+                                </div>
+                            )
+                        } else {
+                            return null;
+                        }
+                    })}
             
                      
                 </div>
